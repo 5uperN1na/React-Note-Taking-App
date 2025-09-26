74045ff70e52de1a7aff5ca29711c2e73d9754cb
psuarez <pauladzuck@hotmail.com>
2025-09-26 16:15:59 -0500

    Adding basic prop drilling.

--- src/Components/NoteForm.jsx
+++ src/Components/NoteForm.jsx
@@ -54,7 +54,7 @@ const NoteForm = ({ notes, setNotes }) => {
         onClick={() => setIsFormVisible(!isFormVisible)}
         className="w-full bg-gray-100 border border-gray-300 text-purple-800 py-2-rounded-lg cursor-pointer hover:bg-purple-200 hover: border-purple-300 transition mb-4"
       >
-        {isFormVisible ? "Hide Form X" : "Add New Note +"}
+        {isFormVisible ? "Hide Form ➖" : "Add New Note ➕"}
       </button>
 
       {isFormVisible && (
